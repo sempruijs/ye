bcbbf5deb6c278d7ddfad6a6585c51eafb21f078
sempruijs <sem@pruijs.net>
2023-05-16 23:52:27 +0200

    Save progress (has run time error)

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Error;
+use serde::Deserialize;
+
+#[derive(Deserialize, Debug)]
+struct Quote {
+    quote: String,
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    let request_url = "https://api.kanye.rest";
+
+    let response = reqwest::get(request_url).await?;
+    let quote: Quote = response.json().await.expect("Failed here");
+    println!("{}", quote.quote);
+
+    Ok(())
 }
